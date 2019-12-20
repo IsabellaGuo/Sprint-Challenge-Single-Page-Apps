ae4c454172bf426734ef511daa2fb7c44b71772b
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-20 08:36:06 -0800

    App and index js file is done for now.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import { Route, Link } from 'react-router-dom';
+
 import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
+import Axios from 'axios';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Link to = {'/'}>
+        <button>Home</button>
+      </Link>
+
+      <Link to = {'/characters'}>
+        <button>Characters</button>
+      </Link>
+
+      <Route exact path="/" >
+        <WelcomePage />
+        </Route>
+        <Route path="/characters">
+         <CharacterList />
+        </Route>
     </main>
   );
 }
